188d7697d98863eb611e9d301e569e1a0e6f3a9b
hirosetino <142306048+hirosetino@users.noreply.github.com>
2023-10-12 17:15:22 +0900

    roomsコントローラー　createアクション追加

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,17 @@
 class RoomsController < ApplicationController
     def index
     end
+
+    def create
+        @room = Room.new(room_params)
+        if @room.save
+            redirect_to root_path
+        end
+    end
+    
+    private
+
+    def room_params
+        params.require(:room).permit(:room_name).merge(user_id: current_user.id)
+    end
 end
