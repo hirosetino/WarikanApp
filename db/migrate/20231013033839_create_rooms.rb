bcb9f940b539a7b970226471f967629b7ff8c69e
hirosetino <142306048+hirosetino@users.noreply.github.com>
2023-10-13 12:41:41 +0900

    roomsテーブル作成

--- db/migrate/20231013033839_create_rooms.rb
+++ db/migrate/20231013033839_create_rooms.rb
@@ -1,7 +1,8 @@
 class CreateRooms < ActiveRecord::Migration[7.0]
   def change
     create_table :rooms do |t|
-
+      t.string :room_name, null: false
+      t.references :user,  null: false, foreign_key: true
       t.timestamps
     end
   end
