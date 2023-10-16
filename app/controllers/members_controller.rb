70ce7e73aad6c8c4b619c6cc3e6085a4b18ee75a
hirosetino <142306048+hirosetino@users.noreply.github.com>
2023-10-16 16:24:35 +0900

    memberコントローラー　createアクション追加

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,14 @@
 class MembersController < ApplicationController
+    def create
+        @member = Member.new(member_params)
+        if @member.save
+            redirect_to room_path(room.id)
+        end
+    end
+
+    private
+
+    def member_params
+        params.permit(:name).merge(room_id: room.id)
+    end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,5 @@
 class Member < ApplicationRecord
     belongs_to :room
-    
+
     validates :name, presence: true
 end
