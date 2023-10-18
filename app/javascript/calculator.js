46a4f85329ce454b7b4624beddda2c18184172ce
hirosetino <142306048+hirosetino@users.noreply.github.com>
2023-10-18 11:40:33 +0900

    showビュー合計金入力フォーム追加

--- app/javascript/calculator.js
+++ app/javascript/calculator.js
@@ -1,14 +1,16 @@
-const display = document.querySelector('.display');
-const buttons = document.querySelectorAll('button');
+document.addEventListener('turbo:load', function () {
+    const display = document.querySelector('.display');
+    const buttons = document.querySelectorAll('button');
 
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    if (button.textContent === '=') {
-      display.value = eval(display.value);
-    } else if (button.textContent === 'C') {
-      display.value = '';
-    } else {
-      display.value += button.textContent;
-    }
-  });
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            if (button.textContent === '=') {
+                display.value = eval(display.value);
+            } else if (button.textContent === 'C') {
+                display.value = '';
+            } else {
+                display.value += button.textContent;
+            }
+        });
+    });
 });
