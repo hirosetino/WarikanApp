eb23307970f59ae47dce1a9992cc0187b0d7ba3d
hirosetino <142306048+hirosetino@users.noreply.github.com>
2023-10-24 09:47:08 +0900

    端数支払い者選択後テキストに表示

---
+++ app/javascript/fraction.js
@@ -0,0 +1,21 @@
+document.addEventListener('turbo:load', function() {
+    const memberButtons = document.querySelectorAll('.select-member');
+
+    memberButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const memberName = button.textContent; // データ属性からメンバーの名前を取得
+            const memberFraction = document.querySelector(`[data-name="${memberName}"]`);
+            console.log(memberFraction.textContent);
+
+            if (memberFraction) {
+                const fractionInput = document.getElementById('fraction');
+                console.log(memberName); // メンバー名の表示
+                console.log(memberFraction.textContent); // フラクション情報のテキストを表示
+                const newFractionValue = Number(fractionInput.value);
+                memberFraction.textContent = `fraction: ${newFractionValue}`;
+            } else {
+                console.log('要素が見つかりませんでした');
+            }
+        });
+    });
+});
