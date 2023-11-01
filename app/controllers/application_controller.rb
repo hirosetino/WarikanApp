7f1d94b83ef06daec1ffe9adfa66bdb949a99d0d
hirosetino <142306048+hirosetino@users.noreply.github.com>
2023-11-01 11:32:43 +0900

    言語変更アクション追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,19 @@
 class ApplicationController < ActionController::Base
+    before_action :set_locale
     before_action :authenticate_user!
     before_action :configure_permitted_parameters, if: :devise_controller?
 
+    def set_locale
+        I18n.locale = session[:locale] || I18n.default_locale
+    end
+
+    def change_locale
+        session[:locale] = params[:locale]
+        redirect_back(fallback_location: root_path)
+    end
+
     private
+
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
     end
