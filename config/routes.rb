05eb4e6cfe11a2bfffb8f27b6d2afb0fdddce617
hirosetino <142306048+hirosetino@users.noreply.github.com>
2023-11-01 11:29:51 +0900

    言語変更 root追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
     devise_for :users
     root 'rooms#index'
-    resources :rooms, only: [:create, :show] do
-        resources :members, only: [:create, :update] 
+    resources :rooms, only: [:create, :show, :destroy] do
+        resources :members, only: [:create, :update, :destroy]
     end
     get '/members/count', to: 'members#member_count'
+    get 'change_locale/:locale', to: 'application#change_locale', as: 'change_locale'
 end
