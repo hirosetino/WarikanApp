a85369a38583e2e84021db1003edea5264e0cf67
hirosetino <142306048+hirosetino@users.noreply.github.com>
2023-11-08 08:33:15 +0900

    random.js修正

---
+++ app/javascript/random.js
@@ -0,0 +1,17 @@
+document.addEventListener('turbo:load', function() {
+    const randomButton = document.getElementById('random');
+  const randomDisplay = document.getElementById('random-display');
+  
+  // Random ボタンのクリックイベントハンドラを追加
+  randomButton.addEventListener('click', function() {
+    // メンバーボタンを取得
+    const memberButtons = document.querySelectorAll('.select-member');
+    
+    // ランダムにメンバーを選択
+    const randomIndex = Math.floor(Math.random() * memberButtons.length);
+    const randomMember = memberButtons[randomIndex].getAttribute('data-member-name');
+    
+    // 結果を表示
+    randomDisplay.textContent = randomMember;
+  });
+});
